c750554bade9e242eb0311ebe3d27ce906342abf
Douglas Regatieri <douglasregatieri2009@gmail.com>
2021-03-25 19:02:48 -0300

    initial code

---
+++ src/pages/Home/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Header(){
+    return(
+        <header className="header">
+        <div className="toolbar">
+           <div>
+              <span>conecta Dev</span>
+           </div>
+           <div className="">
+              <button>Novo Post</button>
+              <span>img1</span>
+              <span>imsg2</span>
+           </div>
+
+        </div>
+     </header>
+    )
+}
+
+export default Header
